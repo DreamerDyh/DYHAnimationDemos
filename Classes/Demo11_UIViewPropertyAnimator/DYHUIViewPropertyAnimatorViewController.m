88888121852b6bc5f6b54852eb4fcdf6a92369d2
Dai Yunhao <hzdaiyunhao@corp.netease.com>
2017-12-07 15:56:52 +0800

    change:aaa

--- Classes/Demo11_UIViewPropertyAnimator/DYHUIViewPropertyAnimatorViewController.m
+++ Classes/Demo11_UIViewPropertyAnimator/DYHUIViewPropertyAnimatorViewController.m
@@ -12,6 +12,10 @@
 
 @property (nonatomic, weak) UIImageView *imageView;
 
+@property (nonatomic, weak) UISlider *slider;
+
+@property (nonatomic, strong) UIViewPropertyAnimator *animator;
+
 @end
 
 @implementation DYHUIViewPropertyAnimatorViewController
@@ -19,6 +23,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self setUpSubviews];
+    //[self setAnimationByTraditionalWay];
+    //[self setAnimationByNewWay];
+    [self quicklySetAnimationWithSelfTimingFunction];
 }
 
 - (void)setUpSubviews
@@ -36,8 +43,45 @@
         make.centerX.centerY.equalTo(self.view);
         make.size.mas_equalTo(CGSizeMake(165.f, 307.f));
     }];
+    
+    UISlider *slider = [[UISlider alloc] initWithFrame:CGRectMake((CGRectGetWidth(self.view.bounds)-200.f)/2.f, CGRectGetHeight(self.view.bounds)-60.f, 200.f, 30.f)];
+    [slider addTarget:self action:@selector(slide:) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:slider];
+    self.slider = slider;
 }
 
+- (void)setAnimationByTraditionalWay
+{
+    CABasicAnimation* rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.y"];
+    rotationAnimation.duration = 1.f;
+    rotationAnimation.fromValue = @0;
+    rotationAnimation.toValue = @(M_PI);
+    [self.imageView.layer addAnimation:rotationAnimation forKey:nil];
+    self.imageView.layer.speed = 0;
+}
+
+- (void)setAnimationByNewWay
+{
+    UIViewPropertyAnimator *animator = [[UIViewPropertyAnimator alloc] initWithDuration:1.f curve:UIViewAnimationCurveEaseOut animations:^{
+        self.imageView.layer.transform = CATransform3DMakeRotation(M_PI, 0, 1, 0);
+    }];
+    [animator pauseAnimation];
+    self.animator = animator;
+}
+
+- (void)slide:(UISlider *)slider
+{
+    //self.imageView.layer.timeOffset = (CFTimeInterval)slider.value;
+    self.animator.fractionComplete = (CGFloat)slider.value;
+}
+
+- (void)quicklySetAnimationWithSelfTimingFunction
+{
+    UIViewPropertyAnimator *animator = [[UIViewPropertyAnimator alloc] initWithDuration:1.f controlPoint1:CGPointMake(0.3, 0.5) controlPoint2:CGPointMake(0.9, 0.5) animations:^{
+        self.imageView.layer.transform = CATransform3DMakeRotation(M_PI, 0, 1, 0);
+    }];
+    [animator startAnimation];
+}
 
 
 @end
